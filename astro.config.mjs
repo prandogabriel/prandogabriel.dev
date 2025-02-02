7f7715c28621e5da5f2bcf8cc18f8f2facf15721
prandogabriel <gprando55@gmail.com>
2025-02-02 13:25:29 -0300

    feat: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()]
+  integrations: [tailwind(), icon(), mdx()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true, // set to false when using @vercel/analytics@1.4.0
+    },
+  }),
 });
