80430af0977e03803e1f1a51b09887b970a55352
prandogabriel <gprando55@gmail.com>
2025-02-02 19:35:33 -0300

    revert: remove vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
@@ -7,9 +6,4 @@ import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   integrations: [tailwind(), icon(), mdx()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true, // set to false when using @vercel/analytics@1.4.0
-    },
-  }),
 });
